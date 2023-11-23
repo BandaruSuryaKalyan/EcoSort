f99cd7fc9e77bd5b9bc3de35e54e3934779a8760
Surya Kalyan <suryakalyan43@gmail.com>
2023-11-23 12:41:51 +0530

    Added Live View (Not Compleated)

---
+++ app/src/main/java/com/suryakalyan/ecosort/FeedAdapter.java
@@ -0,0 +1,2 @@
+package com.suryakalyan.ecosort;public class FeedAdapter {
+}
---
+++ app/src/main/java/com/suryakalyan/ecosort/FeedViewHolder.java
@@ -0,0 +1,38 @@
+package com.suryakalyan.ecosort;// FeedViewHolder.java
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.suryakalyan.ecosort.LocationData;
+
+public class FeedViewHolder extends RecyclerView.ViewHolder {
+    
+    private TextView textViewComplainer, textViewDateTime, textViewLatitude, textViewLongitude, textViewAddress;
+    private ImageView imageViewUpvote, imageViewDownvote, imageViewShare;
+    
+    public FeedViewHolder(@NonNull View itemView) {
+        super(itemView);
+        
+        textViewComplainer = itemView.findViewById(R.id.textViewComplainer);
+        textViewDateTime = itemView.findViewById(R.id.textViewDateTimeValue);
+        textViewLatitude = itemView.findViewById(R.id.textViewLatitudeValue);
+        textViewLongitude = itemView.findViewById(R.id.textViewLongitudeValue);
+        textViewAddress = itemView.findViewById(R.id.textViewAddressValue);
+        imageViewUpvote = itemView.findViewById(R.id.imageViewUpvote);
+        imageViewDownvote = itemView.findViewById(R.id.imageViewDownvote);
+        imageViewShare = itemView.findViewById(R.id.imageViewShare);
+    }
+    
+    public void bindData( LocationData model) {
+        // Bind data to the views
+        textViewComplainer.setText("Complainer: " + model.getTitle());
+        textViewDateTime.setText(model.getSnippet());
+        textViewLatitude.setText("Latitude: " + model.getPosition().latitude);
+        textViewLongitude.setText("Longitude: " + model.getPosition().longitude);
+        textViewAddress.setText("Address: " + model.getAddress());
+        
+    }
+}
